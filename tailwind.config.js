e1d778283fce36ff709fcb742573765fd3c6ebe9
Darthcrowther <michael.crowther@justice.gov.uk>
2025-07-31 12:11:08 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        hmctsBlue: '#1d70b8',
+        hmctsGray: '#f3f2f1',
+      },
+    },
+  },
+  plugins: [],
+}
